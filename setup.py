9e3dde564aa4824f2c1ea22becce74e1b262ca10
mvinyard <mvinyard@broadinstitute.org>
2021-11-12 21:25:33 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import re, os, sys
+
+setup(
+    name="vinplots",
+    version="0.0.1",
+    python_requires=">3.8.0",
+    author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
+    author_email="mvinyard@broadinstitute.org",
+    url="https://github.com/mvinyard/vinplots",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    description="crispr_tools - Analysis Tools for CRISPR Screen Design and Analysis",
+    packages=[
+        "vinplots",
+    ],
+    install_requires=[
+        "matplotlib>=3.4",
+        "numpy>=1.19.2",
+        "pandas>=1.1.2",
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    license="MIT",
+)
---
+++ vinplots/__init__.py
@@ -0,0 +1,6 @@
+# main __init__.py
+
+# __init__.py
+__module_name__ = "__init__.py"
+__author__ = ", ".join(["Michael E. Vinyard"])
+__email__ = ", ".join(["vinyard@g.harvard.edu",])
