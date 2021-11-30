4961ff3f69e24f942918564fe9d5f787241f8669
mvinyard <mvinyard@broadinstitute.org>
2021-11-30 15:57:08 +0000

    add SHARE-seq color palette

---
+++ vinplots/_color_palettes/_ColorPalettesModule.py
@@ -0,0 +1,15 @@
+from ._palettes._SHAREseq import _SHAREseq
+
+class _ColorPalettes:
+    
+    def __init__(self):
+        
+        """"""
+        
+    def SHAREseq(self):
+        
+        """"""
+        
+        share_seq = _SHAREseq()
+        return share_seq.colors
+        
