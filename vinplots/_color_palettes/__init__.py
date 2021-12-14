a08a0e49ba0afb5fa3320b03fa57cb55f03da301
mvinyard <mvinyard@broadinstitute.org>
2021-12-14 19:32:38 +0000

    fix typo

--- vinplots/_color_palettes/__init__.py
+++ vinplots/_color_palettes/__init__.py
@@ -1,4 +1,4 @@
 # __init__.py
 
 from ._palettes._SHAREseqModule import _SHAREseq_palette as SHAREseq
-from ._palettes._warm_palette improt _warm_palette as warm
+from ._palettes._warm_palette import _warm_palette as warm
