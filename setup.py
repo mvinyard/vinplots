0fcea084d94710649ad094782ea0041edc343c9f
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-03-04 20:27:39 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import re, os, sys
 
 setup(
     name="vinplots",
-    version="0.0.21",
+    version="0.0.22",
     python_requires=">3.7.0",
     author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
     author_email="mvinyard@broadinstitute.org",
     url="https://github.com/mvinyard/vinplots",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    description="crispr_tools - Analysis Tools for CRISPR Screen Design and Analysis",
+    description="vinplots - plotting assistant",
     packages=[
         "vinplots",
     ],
