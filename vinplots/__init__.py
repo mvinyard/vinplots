966d8bf7799bb4c731e5b8b2bc4c19aae7b8d3d2
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-03-04 22:48:08 -0700

    Update __init__.py

--- vinplots/__init__.py
+++ vinplots/__init__.py
@@ -4,20 +4,30 @@ __module_name__ = "__init__.py"
 __author__ = ", ".join(["Michael E. Vinyard"])
 __email__ = ", ".join(["vinyard@g.harvard.edu",])
 
-
 # package imports #
 # --------------- #
 import matplotlib as _mpl
 import matplotlib.font_manager as _font_manager
 import os as _os
+import subprocess as _subprocess
 
+def _download_mscore_fonts():
+    
+    """"""
+    
+    check_package = _subprocess.run(['conda', 'search', 'mscore'], stdout=subprocess.PIPE, stderr=False).stdout.decode().split('\n')
+    if len(check_package) > 3:
+        return "installed"
+    else:
+        download_log = _subprocess.run(['conda', 'install', '-c', 'conda-forge', 'mscorefonts', '-y'], stdout=subprocess.PIPE, stderr=False)
+        return download_log
 
 # matplotlib rcParams #
 # ------------------- #
 
 # install mscorefonts and clear the previous parameter cache file.
 _os.system("conda install -c conda-forge mscorefonts -y")
-_os.system("rm ~/.cache/matplotlib -rf")
+_mscore_install = _download_mscore_fonts()
 
 _font = {"size": 12}
 _mpl.rc(_font)
