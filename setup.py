87cdfb56d523722489c250df5509c8b3c0167f37
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-03-04 22:28:06 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import re, os, sys
 
 setup(
     name="vinplots",
-    version="0.0.35",
+    version="0.0.36",
     python_requires=">3.7.0",
     author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
     author_email="mvinyard@broadinstitute.org",
@@ -14,9 +14,12 @@ setup(
     packages=[
         "vinplots",
         "vinplots._color_palettes",
-        "vinplots._construction",
+        "vinplots._color_palettes._palettes",
+        "vinplots._construction"
+        "vinplots._construction._funcs",
         "vinplots._plot",
         "vinplots._style",
+        "vinplots._style._funcs",
     ],
     install_requires=[
         "matplotlib>=3.4",
