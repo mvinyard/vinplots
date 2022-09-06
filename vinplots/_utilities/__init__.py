f25ac3eddfedd5f98febb1df81ae1aa5219a755f
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-09-06 09:54:42 -0600

    Update __init__.py

--- vinplots/_utilities/__init__.py
+++ vinplots/_utilities/__init__.py
@@ -6,3 +6,4 @@ __email__ = ", ".join(["vinyard@g.harvard.edu",])
 
 # -----------------------------------------------------------------------------
 from ._setup_matplotlib_params import _setup_matplotlib_params as set_plt_params
+from ._fetch_color_palettes import _fetch_color_palettes as fetch_color_palettes
