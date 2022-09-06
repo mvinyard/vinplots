a6303f74fb2571089e8ee6e0ba7810a5b804e573
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-09-06 09:57:33 -0600

    Update __init__.py

--- vinplots/__init__.py
+++ vinplots/__init__.py
@@ -4,17 +4,22 @@ __author__ = ", ".join(["Michael E. Vinyard"])
 __email__ = ", ".join(["vinyard@g.harvard.edu",])
 
 
-# -----------------------------------------------------------------------------
+# import API ------------------------------------------------------------------
 from . import _construction as build
 from . import _style as style
 from . import _utilities as ut
 
-
-# -----------------------------------------------------------------------------
+# import main API functions ---------------------------------------------------
 from ._plot._Plot import _Plot as Plot
 from ._plot._quick_plot import _quick_plot as quick_plot
 from ._construction._save_figure import _save_figure as save
 
-# -----------------------------------------------------------------------------
+# fetch color palettes --------------------------------------------------------
+import os as _os
+url = "https://github.com/mvinyard/vinplots/raw/main/vinplots/_color_palettes/_palette_pkl_src/"
+
+dest = _os.path.join(__file__, _color_palettes/_palette_pkl_src/)
+ut.fetch_color_palettes(url, dest_dir=dest)
+
 from ._color_palettes._ColorPalettes import _ColorPalettes
 colors = _ColorPalettes()
