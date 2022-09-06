a578cef61246b0bb2b63123c31c2643eae6e983a
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-09-05 22:03:48 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import re, os, sys
 
 setup(
     name="vinplots",
-    version="0.0.54",
+    version="0.0.55",
     python_requires=">3.7.0",
     author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
     author_email="mvinyard@broadinstitute.org",
